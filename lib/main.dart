b7bd546bc30fb73f8dd2b0eacf18ac6e5dc183a8
mh-mobile <mh.mobiler@gmail.com>
2021-03-21 18:18:59 +0900

    FlutterSecureStoreでjwtの認証情報を保持

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 import 'my_app.dart';
 import 'ui/home.dart';
+
+final storage = FlutterSecureStorage();
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
