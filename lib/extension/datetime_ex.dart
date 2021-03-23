63a6cb9e106c1d9a537263f81f7e30136bf80c89
mh-mobile <mh.mobiler@gmail.com>
2021-03-23 15:03:21 +0900

    イベント日付のフォーマット変換

--- lib/extension/datetime_ex.dart
+++ lib/extension/datetime_ex.dart
@@ -4,4 +4,8 @@ extension DateTimeExtension on DateTime {
   String convertToTweetDateFormat() {
     return DateFormat("yyyy/MM/dd HH:mm").format(this);
   }
+
+  String convertToEventDateFormat() {
+    return DateFormat("M/dd").format(this);
+  }
 }
