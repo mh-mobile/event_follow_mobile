640392b6a09e706ccc6d9d6dda2e9bb2fcd0623e
mh-mobile <mh.mobiler@gmail.com>
2021-03-25 15:08:43 +0900

    ソート&絞り込みボタンの状態管理

--- lib/ui/sort_filter_button.dart
+++ lib/ui/sort_filter_button.dart
@@ -1,8 +1,34 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SortFilterButton extends StatefulWidget {
+enum SortType {
+  FriendsNumber,
+  RecentOrder,
+  PostedOrder,
+  ClosenessOrder,
+}
 
+enum FriendsFilterType {
+  OneOrMoreFriends,
+  TwoOrMoreFriends,
+  ThreeOrMoreFriends,
+  FourOrMoreFriends,
+  FiveOrMoreFriends,
+}
+
+enum TimeFilterType {
+  EightHours,
+  TwentyFourHour,
+  TwoDays,
+  ThreeDays,
+  FourDays,
+  FiveDays,
+  SixDays,
+  OneWeek,
+  All,
+}
+
+class SortFilterButton extends StatefulWidget {
   const SortFilterButton({
     required this.onTap,
     Key? key,
@@ -11,11 +37,41 @@ class SortFilterButton extends StatefulWidget {
   final VoidCallback onTap;
 
   @override
-  State<StatefulWidget> createState() => _SortFilterButtonState();
+  State<StatefulWidget> createState() => SortFilterButtonState();
 }
 
-class _SortFilterButtonState extends State<SortFilterButton> {
+class SortFilterButtonState extends State<SortFilterButton> {
   bool _pressing = false;
+  SortType? selectedSortType;
+  FriendsFilterType? selectedFriendsType;
+  TimeFilterType? selectedTimeType;
+
+  final sortLabels = [
+    ["Friend", "数"],
+    ["新着", "順"],
+    ["投稿", "順"],
+    ["開催が近い", "順"],
+  ];
+
+  final friendsFilterLabels = [
+    ["Friends", "1+"],
+    ["Friends", "2+"],
+    ["Friends", "3+"],
+    ["Friends", "4+"],
+    ["Friends", "5+"],
+  ];
+
+  final timeFilterLabels = [
+    ["8", "hrs"],
+    ["24", "hrs"],
+    ["2", "days"],
+    ["3", "hrs"],
+    ["4", "hrs"],
+    ["5", "hrs"],
+    ["6", "hrs"],
+    ["1", "week"],
+    ["All"],
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -37,45 +93,86 @@ class _SortFilterButtonState extends State<SortFilterButton> {
         });
       },
       onTap: widget.onTap,
-      child: Padding(
-        padding: EdgeInsets.all(6),
-        child: AnimatedOpacity(
-          opacity: _pressing ? 0.4 : 1,
-          duration: Duration(milliseconds: 160),
-          child: Container(
-            width: 100,
-            height: 44,
-            decoration: BoxDecoration(
-              color: Colors.transparent,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("開催が", style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),),
-                    Text("近い順", style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),),
-                  ],
+      child: selectedSortType == null
+          ? SizedBox.shrink()
+          : Padding(
+              padding: EdgeInsets.all(6),
+              child: AnimatedOpacity(
+                opacity: _pressing ? 0.4 : 1,
+                duration: Duration(milliseconds: 160),
+                child: Container(
+                  width: 100,
+                  height: 44,
+                  decoration: BoxDecoration(
+                    color: Colors.transparent,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: _createSortList(),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(left: 5, right: 5),
+                        child: Text(
+                          "x",
+                          style: TextStyle(
+                              fontSize: 10, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: _createFilterList(),
+                      )
+                    ],
+                  ),
                 ),
-                Container(
-                  margin: EdgeInsets.only(left: 5, right: 5),
-                  child: Text("x", style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),),
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Friends", style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),),
-                    Text("5+", style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),),
-                  ],
-                )
-              ],
+              ),
             ),
-          ),
-        ),
-      ),
-
     );
   }
 
+  List<Text> _createSortList() {
+    final labels = sortLabels[selectedSortType!.index];
+    return labels
+        .map((label) => Text(label,
+            style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold)))
+        .toList();
+  }
+
+  List<Text> _createFilterList() {
+    if (selectedSortType == SortType.FriendsNumber) {
+      return _createTimeFilterList();
+    }
+
+    return _createFriendsFilterList();
+  }
+
+  List<Text> _createFriendsFilterList() {
+    final labels = friendsFilterLabels[selectedFriendsType!.index];
+    return labels
+        .map((label) => Text(label,
+            style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold)))
+        .toList();
+  }
+
+  List<Text> _createTimeFilterList() {
+    final labels = timeFilterLabels[selectedTimeType!.index];
+    return labels
+        .map((label) => Text(label,
+            style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold)))
+        .toList();
+  }
+
+  void setCondition(
+      {required SortType sortType,
+      FriendsFilterType? friendsFilterType,
+      TimeFilterType? timeFilterType}) {
+    setState(() {
+      this.selectedSortType = sortType;
+      this.selectedFriendsType = friendsFilterType;
+      this.selectedTimeType = timeFilterType;
+    });
+  }
 }
