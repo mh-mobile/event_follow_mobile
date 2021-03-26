1d45b8e4174192688b5239193df86720999af2d5
mh-mobile <mh.mobiler@gmail.com>
2021-03-26 21:41:52 +0900

    feat: flutter_dotenvでTwitterの秘密情報を.envから読み取る処理を追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
+
 
 import 'my_app.dart';
 
@@ -9,6 +11,7 @@ final firebaseAuth = FirebaseAuth.instance;
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await DotEnv.load();
   runApp(MyApp());
 }
 
