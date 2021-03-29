2238392fccb5f45adc8df2afb737242371c6a089
mh-mobile <mh.mobiler@gmail.com>
2021-03-30 03:41:26 +0900

    refactor: ホーム画面のTwitterログインボタンをTwitterLoginButtonコンポーネントに変更

--- lib/pages/home_pages/home_page.dart
+++ lib/pages/home_pages/home_page.dart
@@ -1,16 +1,10 @@
-import 'package:event_follow/main.dart';
-import 'package:event_follow/pages/events_pages/events_page.dart';
 import 'package:event_follow/pages/home_pages/home_footer.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:event_follow/pages/home_pages/twitter_login_button.dart';
 
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:http/http.dart' as http;
-import 'package:twitter_login/twitter_login.dart';
 import 'dart:convert';
 
 import 'home_logo.dart';
@@ -42,60 +36,7 @@ class HomePage extends HookWidget {
                     flex: 2,
                     child: Container()
                 ),
-                !isLoading ? Container(
-                  height: 44,
-                  child: ElevatedButton.icon(
-                    icon: Image.asset("assets/twitter_logo.png", height: 25,),
-                    label: Text("Twitterでログイン"),
-                    onPressed: () async {
-                      final twitterLogin = TwitterLogin(
-                          apiKey: env["TWITTER_API_KEY"],
-                          apiSecretKey: env["TWITTER_API_SECRET_KEY"],
-                          redirectURI: env["TWITTER_REDIRECT_RUI"]
-                      );
-                      final authResult = await twitterLogin.login();
-                      switch (authResult.status) {
-                        case TwitterLoginStatus.loggedIn:
-                        // setState(() {
-                        //   isLoading = true;
-                        // });
-                          final credential = TwitterAuthProvider.credential(
-                              accessToken: authResult.authToken,
-                              secret: authResult.authTokenSecret);
-                          final firebaseCredential = await firebaseAuth.signInWithCredential(credential);
-
-                          final idToken = await firebaseCredential.user?.getIdToken();
-
-                          final request = SessionApiRequest(
-                              token: idToken!,
-                              accessToken: authResult.authToken,
-                              accessTokenSecret: authResult.authTokenSecret);
-
-                          final sessionApiResults =
-                          await requestSessionApi(request: request);
-                          if (sessionApiResults.status == "OK") {
-                            Navigator.pushReplacement(context,
-                                MaterialPageRoute(builder: (context) {
-                                  return EventsPage();
-                                }));
-                          }
-
-                          break;
-                        case TwitterLoginStatus.cancelledByUser:
-                        // setState(() {
-                        //   isLoading = false;
-                        // });
-                          break;
-                        case TwitterLoginStatus.error:
-                        // setState(() {
-                        //   isLoading = false;
-                        // });
-                          break;
-                      }
-
-                    },
-                  ),
-                ) : Container(
+                !isLoading ? TwitterLoginButton() : Container(
                     height: 44,
                     width: 44,
                     child: CircularProgressIndicator()
