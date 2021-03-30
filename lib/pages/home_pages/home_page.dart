36a818d1d76ba1482c482bcd1bdaf4eea62fa060
mh-mobile <mh.mobiler@gmail.com>
2021-03-30 14:17:45 +0900

    feat: riverpodを使ったTwitterログイン実装

--- lib/pages/home_pages/home_page.dart
+++ lib/pages/home_pages/home_page.dart
@@ -1,106 +1,64 @@
+import 'package:event_follow/models/controllers/sessions_controller/sessions_controller.dart';
+import 'package:event_follow/models/controllers/sessions_controller/sessions_state.dart';
+import 'package:event_follow/pages/events_pages/events_page.dart';
 import 'package:event_follow/pages/home_pages/home_footer.dart';
 import 'package:event_follow/pages/home_pages/twitter_login_button.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'home_logo.dart';
 
-bool isLoading = false;
+final _shouldPush = Provider.autoDispose(
+    (ref) => ref.watch(sessionsProvider.state).status == SessionsStatus.OK);
 
 class HomePage extends HookWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white.withOpacity(0.0),
-        elevation: 0.0,
-      ),
-      body: SafeArea(
-        child: Center(
-          child: Container(
-            width: MediaQuery.of(context).size.width * 6/7,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Expanded(
-                    flex: 1,
-                    child: Container()
-                ),
-                HomeLogo(),
-                Expanded(
-                    flex: 2,
-                    child: Container()
-                ),
-                !isLoading ? TwitterLoginButton() : Container(
-                    height: 44,
-                    width: 44,
-                    child: CircularProgressIndicator()
-                ),
-                Expanded(
-                    flex: 3,
-                    child: Container()
-                ),
-                HomeFooter(),
-              ],
+    final isLoading =
+        useProvider(sessionsProvider.state.select((value) => value.isLoading));
+
+    return ProviderListener(
+      provider: _shouldPush,
+      onChange: (context, bool shouldPush) {
+        if (shouldPush) {
+          Navigator.pushReplacement(context,
+              MaterialPageRoute(builder: (context) {
+                return EventsPage();
+              }));
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white.withOpacity(0.0),
+          elevation: 0.0,
+        ),
+        body: SafeArea(
+          child: Center(
+            child: Container(
+              width: MediaQuery.of(context).size.width * 6 / 7,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(flex: 1, child: Container()),
+                  HomeLogo(),
+                  Expanded(flex: 2, child: Container()),
+                  !isLoading
+                      ? TwitterLoginButton()
+                      : Container(
+                          height: 44,
+                          width: 44,
+                          child: CircularProgressIndicator()),
+                  Expanded(flex: 3, child: Container()),
+                  HomeFooter(),
+                ],
+              ),
             ),
           ),
         ),
       ),
     );
-  }
-}
-
-
-Future<SessionApiResults> requestSessionApi(
-    {required SessionApiRequest request}) async {
-  final url =
-  Uri.parse("https://event-follow-front.herokuapp.com/api/sessions");
-  final response = await http.post(
-    url,
-    body: json.encode(request.toJson()),
-    headers: {"Content-Type": "application/json"},
-  );
-
-  if (response.statusCode == 200) {
-    return SessionApiResults.fromJson(json.decode(response.body));
-  } else {
-    throw Exception("Login Failed");
-  }
-}
-
-class SessionApiRequest {
-  final String token;
-  final String accessToken;
-  final String accessTokenSecret;
-
-  SessionApiRequest({
-    required this.token,
-    required this.accessToken,
-    required this.accessTokenSecret,
-  });
-
-  Map<String, dynamic> toJson() => {
-    "token": this.token,
-    "access_token": this.accessToken,
-    "access_token_secret": this.accessTokenSecret,
-  };
-}
-
-class SessionApiResults {
-  final String status;
-  final String? message;
-
-  SessionApiResults({
-    required this.status,
-    this.message,
-  });
-
-  factory SessionApiResults.fromJson(Map<String, dynamic> json) {
-    return SessionApiResults(status: json["status"], message: json["message"]);
   }
 }
