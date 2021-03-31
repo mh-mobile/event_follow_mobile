e1b90ed70436907ac8f26b4d15436e6a399cdf7d
mh-mobile <mh.mobiler@gmail.com>
2021-04-01 05:58:59 +0900

    fix: イベント一覧画面のソート&絞り込みボタンが表示されない不具合修正

--- lib/models/controllers/events_controller/events_controller.dart
+++ lib/models/controllers/events_controller/events_controller.dart
@@ -1,9 +1,12 @@
+import 'package:event_follow/config/sort_filter_globals.dart';
 import 'package:event_follow/models/repositories/events/events_repository.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import '../../../main.dart';
 import 'events_state.dart';
 
 export 'events_state.dart';
+
+final eventsConditionProvider = StateProvider.autoDispose<SortFilterStateStore?>((ref) => null);
 
 final eventsProvider =
     StateNotifierProvider.autoDispose((ref) => EventsController(ref.read));
@@ -34,6 +37,13 @@ class EventsController extends StateNotifier<EventsState> {
 
     final eventsApiResults =
         await _eventsRepository.requestEventsApi(request: request);
+
+    this._read(eventsConditionProvider).state = SortFilterStateStore(
+      sortType: SortTypeExtension.convert(eventsApiResults.meta.eventSortType),
+      timeFilterType: TimeFilterTypeExtension.convert(eventsApiResults.meta.timeFilterType),
+      friendFilterType: FriendsFilterTypeExtension.convert(eventsApiResults.meta.friendsFilterType),
+    );
+
     state = state.copyWith(
       data: [...state.data, ...eventsApiResults.data],
       meta: eventsApiResults.meta,
