b07ba741cc734836b05a2a2bb0e662e7f288ab06
mh-mobile <mh.mobiler@gmail.com>
2021-04-02 02:04:04 +0900

    reafactor: イベント一覧のコンテンツ部分をコンポーネント化

---
+++ lib/pages/events_pages/event_content_view.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+import 'event_card.dart';
+
+class EventContentView extends HookWidget {
+
+  final onRefresh;
+  final scrollController;
+  final List<EventCard> cardList;
+
+  EventContentView({
+    required this.onRefresh,
+    required this.scrollController,
+    required this.cardList,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      padding: EdgeInsets.all((8)),
+      child: RefreshIndicator(
+        onRefresh: onRefresh,
+        child: ListView.builder(
+            itemCount: cardList.length,
+            shrinkWrap: true,
+            physics: const AlwaysScrollableScrollPhysics(),
+            controller: scrollController,
+            itemBuilder: (context, index) {
+              return cardList[index];
+            }),
+      ),
+    );
+  }
+
+}
--- lib/pages/events_pages/event_listview.dart
+++ lib/pages/events_pages/event_listview.dart
@@ -1,12 +1,12 @@
 import 'package:event_follow/models/controllers/events_controller/events_controller.dart';
 import 'package:event_follow/models/repositories/events/events_repository.dart';
+import 'package:event_follow/pages/events_pages/event_content_view.dart';
 import 'package:event_follow/pages/events_pages/event_empty_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import '../../config/sort_filter_globals.dart';
 import 'event_card.dart';
-import 'no_empty_logo.dart';
 
 class EventListView extends HookWidget {
   const EventListView({
@@ -62,24 +62,18 @@ class EventListView extends HookWidget {
       return EventEmptyView(onRefresh: _onRefresh);
     }
 
-    return (isLoading && data.length == 0)
-        ? Center(
-      child: CircularProgressIndicator(),
-    )
-        : Container(
-      padding: EdgeInsets.all((8)),
-      child: RefreshIndicator(
-        onRefresh: _onRefresh,
-        child: ListView.builder(
-            itemCount: _cardList.length,
-            shrinkWrap: true,
-            physics: const AlwaysScrollableScrollPhysics(),
-            controller: _scrollController,
-            itemBuilder: (context, index) {
-              return _cardList[index];
-            }),
-      ),
+    if (isLoading && data.length == 0) {
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    return EventContentView(
+      onRefresh: _onRefresh,
+      scrollController: _scrollController,
+      cardList: _cardList
     );
+
   }
 
   bool _hasNextPaging(int currentPage, int totalPages) {
