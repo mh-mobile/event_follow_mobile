08806509802065f8ff988c95ffed3e06ff47a83d
mh-mobile <mh.mobiler@gmail.com>
2021-04-01 22:29:03 +0900

    reactor: イベント一覧のDrawerを別コンポーネントに切り出し

---
+++ lib/pages/events_pages/event_drawer_header.dart
@@ -0,0 +1,69 @@
+import 'package:event_follow/pages/home_pages/home_page.dart';
+import 'package:event_follow/pages/setting_pages/setting_page.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../main.dart';
+
+class EventDrawerHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                    height: 60,
+                    child: Container(
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(50),
+                          child: Image.network(
+                            firebaseAuth.currentUser!.photoURL!,
+                            fit: BoxFit.cover,
+                          )),
+                    )),
+                Container(
+                  margin: EdgeInsets.only(top: 10.0),
+                  child: Text(
+                    firebaseAuth.currentUser!.displayName!,
+                    style: TextStyle(color: Colors.white),
+                  ),
+                )
+              ],
+            ),
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+          ),
+          ListTile(
+            title: Text("設定"),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return SettingPage();
+              }));
+            },
+          ),
+          ListTile(
+            title: Text("ログアウト"),
+            onTap: () {
+              firebaseAuth.signOut();
+              Navigator.pushReplacement(
+                context,
+                PageRouteBuilder(
+                  pageBuilder: (context, _, __) => HomePage(),
+                  transitionDuration: Duration(seconds: 0),
+                ),
+              );
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
