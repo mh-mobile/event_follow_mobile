ed2ce2c0646a4541a4c555797c39a8a694596b4e
mh-mobile <mh.mobiler@gmail.com>
2021-04-02 02:07:58 +0900

    refactor: listviewの名称をlist_viewに変更

--- lib/pages/events_pages/events_page.dart
+++ lib/pages/events_pages/events_page.dart
@@ -7,7 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import '../../config/sort_filter_globals.dart';
-import 'event_listview.dart';
+import 'event_list_view.dart';
 
 final sortFilterStateKey = GlobalKey<SortFilterButtonState>();
 
--- lib/pages/events_pages/friends_footer.dart
+++ lib/pages/events_pages/friends_footer.dart
@@ -7,7 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'following_tweets_listview.dart';
+import 'following_tweets_list_view.dart';
 
 class FriendsFooter extends HookWidget {
   final Event _event;
