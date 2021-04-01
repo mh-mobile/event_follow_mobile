674d9bb3aec94263869aea550703a4950870f080
mh-mobile <mh.mobiler@gmail.com>
2021-04-02 01:46:57 +0900

    refactor: イベント一覧のNo Imageのコンポーネント化

---
+++ lib/pages/events_pages/event_empty_view.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+import 'no_empty_logo.dart';
+
+class EventEmptyView extends HookWidget {
+  final onRefresh;
+
+  EventEmptyView({
+    required this.onRefresh,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return RefreshIndicator(
+        onRefresh: this.onRefresh,
+        child: SingleChildScrollView(
+            physics: AlwaysScrollableScrollPhysics(),
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  NoEmptyLogo(),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Text(
+                    "イベントがまだありません",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              height: MediaQuery.of(context).size.height - 200,
+              width: MediaQuery.of(context).size.width,
+            )));
+  }
+}
