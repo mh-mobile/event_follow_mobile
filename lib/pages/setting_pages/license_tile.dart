8d0d48b63f7f59f94b59a8746cd75041d6b363cc
mh-mobile <mh.mobiler@gmail.com>
2021-04-02 16:05:45 +0900

    feat: 設定画面にライセンス項目を追加

---
+++ lib/pages/setting_pages/license_tile.dart
@@ -0,0 +1,23 @@
+import 'package:event_follow/utils/app_utils.dart';
+import 'package:flutter/material.dart';
+
+class LicenseTile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () async {
+        showAboutDialog(
+          context: context,
+          applicationName: await AppUtils.getAppName(),
+          applicationVersion: await AppUtils.getAppVersion(),
+          applicationIcon: await AppUtils.getAppIcon(),
+          applicationLegalese: await AppUtils.getAppLegalese(),
+        );
+      },
+      child: ListTile(
+        title: Text("ライセンス", ),
+        dense: true,
+      ),
+    );
+  }
+}
--- lib/utils/app_utils.dart
+++ lib/utils/app_utils.dart
@@ -1,8 +1,22 @@
+import 'package:flutter/material.dart';
 import 'package:package_info/package_info.dart';
 
 class AppUtils {
   static Future<String> getAppVersion() async {
     final PackageInfo packageInfo = await PackageInfo.fromPlatform();
     return packageInfo.version;
+  }
+
+  static Future<String> getAppName() async {
+    final PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    return packageInfo.appName;
+  }
+
+  static Future<Image> getAppIcon({double width = 40}) async {
+    return Image.asset("assets/logo_notext.png", width: width);
+  }
+
+  static Future<String> getAppLegalese() async {
+    return "2021 mh-mobile";
   }
 }
