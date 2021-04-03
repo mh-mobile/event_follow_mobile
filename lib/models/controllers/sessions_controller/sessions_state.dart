b2687f4ae946fb698bcae24044ef34f74e52b49d
mh-mobile <mh.mobiler@gmail.com>
2021-04-03 23:10:35 +0900

    fix: ログインエラーのハンドリング処理
    エラー時に何も起きないため、エラーアラートを表示するようにした。

--- lib/models/controllers/sessions_controller/sessions_state.dart
+++ lib/models/controllers/sessions_controller/sessions_state.dart
@@ -4,14 +4,15 @@ import 'package:flutter/foundation.dart';
 part 'sessions_state.freezed.dart';
 
 enum SessionsStatus {
+  NONE,
   OK,
-  NG
+  NG,
 }
 
 @freezed
 abstract class SessionsState with _$SessionsState {
   factory SessionsState({
-    @Default(SessionsStatus.NG) SessionsStatus status,
+    @Default(SessionsStatus.NONE) SessionsStatus status,
     @Default(false) bool isLoading,
   }) = _SessionsState;
 
