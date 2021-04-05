5aa2d4e87ec92091d2c67407c4de51004f3988ad
mh-mobile <mh.mobiler@gmail.com>
2021-04-05 16:40:26 +0900

    fix: エラー対応: The argument type 'String?' can't be assigned to the parameter lib/pages/home_pages/twitter_login_button.dart:37)

--- lib/pages/home_pages/twitter_login_button.dart
+++ lib/pages/home_pages/twitter_login_button.dart
@@ -25,16 +25,16 @@ class TwitterLoginButton extends HookWidget {
         label: const Text('Twitterでログイン'),
         onPressed: () async {
           final twitterLogin = TwitterLogin(
-              apiKey: env['TWITTER_API_KEY'],
-              apiSecretKey: env['TWITTER_API_SECRET_KEY'],
-              redirectURI: env['TWITTER_REDIRECT_RUI']);
+              apiKey: env['TWITTER_API_KEY']!,
+              apiSecretKey: env['TWITTER_API_SECRET_KEY']!,
+              redirectURI: env['TWITTER_REDIRECT_RUI']!);
           final authResult = await twitterLogin.login();
           switch (authResult.status) {
             case TwitterLoginStatus.loggedIn:
               controller.setLoading(isLoading: true);
               final credential = TwitterAuthProvider.credential(
-                  accessToken: authResult.authToken,
-                  secret: authResult.authTokenSecret);
+                  accessToken: authResult.authToken!,
+                  secret: authResult.authTokenSecret!);
               final firebaseCredential =
                   await firebaseAuth.signInWithCredential(credential);
 
@@ -42,8 +42,8 @@ class TwitterLoginButton extends HookWidget {
 
               final request = SessionsApiRequest(
                   token: idToken!,
-                  accessToken: authResult.authToken,
-                  accessTokenSecret: authResult.authTokenSecret);
+                  accessToken: authResult.authToken!,
+                  accessTokenSecret: authResult.authTokenSecret!);
               await controller.request(request);
               break;
             case TwitterLoginStatus.cancelledByUser:
