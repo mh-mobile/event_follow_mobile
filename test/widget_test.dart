9ad3f9cbe5b5ad32cb96ddead306d7fec8ea08f2
mh-mobile <mh.mobiler@gmail.com>
2021-04-05 10:38:34 +0900

    fix: 警告対応: Unused import: 'package:event_follow/main.dart'.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,8 +9,6 @@ import 'package:event_follow/my_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:event_follow/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
