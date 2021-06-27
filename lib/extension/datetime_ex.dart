212e8b2ddc54f25b805c5ab920d6e46685b5bfac
mh-mobile <mh.mobiler@gmail.com>
2021-06-27 20:39:56 +0900

    fix: イベント開催日の表記 (#175)

--- lib/extension/datetime_ex.dart
+++ lib/extension/datetime_ex.dart
@@ -2,10 +2,10 @@ import 'package:intl/intl.dart';
 
 extension DateTimeExtension on DateTime {
   String convertToTweetDateFormat() {
-    return DateFormat('yyyy/MM/dd HH:mm').format(this);
+    return DateFormat('yyyy/MM/dd HH:mm').format(toLocal());
   }
 
   String convertToEventDateFormat() {
-    return DateFormat('M/d').format(this);
+    return DateFormat('M/d').format(toLocal());
   }
 }
